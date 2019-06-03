0db7e124c8a2fdf4c80ffbb7f12aa22ae44ec1e7
James Walling <james.walling@bryancave.com>
2019-06-03 09:49:23 -0500

    Finished Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -11,20 +13,41 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            double percent = .2;
+            double percent = 0.2;
             int numStudents = Students.Count;
             int minStudent = 5;
-            double twentyPercent = numStudents * percent;
+            int threshold = (int)Math.Ceiling(numStudents * percent);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            char gradeLetterReturn;
             if (numStudents>=minStudent)
             {
-                if
-                return char.Parse("F");
+                if (grades[threshold - 1] <= averageGrade)
+                {
+                    gradeLetterReturn = 'A';
+                }
+                else if (grades[(threshold * 2) - 1] <= averageGrade)
+                {
+                    gradeLetterReturn = 'B';
+                }
+                else if (grades[(threshold * 3) - 1] <= averageGrade)
+                {
+                    gradeLetterReturn = 'C';
+                }
+                else if (grades[(threshold * 4) - 1] <= averageGrade)
+                {
+                    gradeLetterReturn = 'D';
+                }
+                else
+                {
+                    gradeLetterReturn = 'F';
+                }
             }
             else
             {
-                throw new System.InvalidOperationException("Not enough students, need at least "+minStudent+" students");
+                throw new InvalidOperationException("Not enough students, need at least "+minStudent+" students");
             }
-            
+            return gradeLetterReturn;
+
         }
 
        
